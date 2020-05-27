50018eeeb4be4efc6e69b387d646375786c544b8
Noble Obioma <nobioma1@gmail.com>
2020-05-27 10:25:06 +0100

    Create migration for user with fields: - id - firstName - lastName - walletBalance - createdAt -updatedAt

---
+++ server/sequelize/migrations/20200525202527-create-user.js
@@ -0,0 +1,41 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('users', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      firstName: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      lastName: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      walletBalance: {
+        allowNull: false,
+        type: Sequelize.FLOAT,
+        defaultValue: 0,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: new Date(),
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: new Date(),
+      },
+    });
+  },
+
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('users');
+  },
+};
