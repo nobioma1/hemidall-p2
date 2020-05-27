f418cc6bdace2d9080c3d90d954e6d3bcc88787a
Noble Obioma <nobioma1@gmail.com>
2020-05-27 11:38:27 +0100

    Create Header Component

---
+++ client/src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import { Nav } from './Nav';
+
+export const Header = () => {
+  return (
+    <div className="header">
+      <h1>Barza Bank</h1>
+      <Nav />
+    </div>
+  );
+};
