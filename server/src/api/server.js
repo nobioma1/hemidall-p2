4fe8747563f2b350c1f4b369ae2cb92ca31fd067
Noble Obioma <nobioma1@gmail.com>
2020-05-27 11:33:06 +0100

    Add routes and error-handler middleware

--- server/src/api/server.js
+++ server/src/api/server.js
@@ -1,9 +1,19 @@
 const cors = require('cors');
 const express = require('express');
 
+const getUsers = require('../routes/get-users');
+const createUser = require('../routes/create-user');
+const userTransactions = require('../routes/user-transactions');
+const errorHandler = require('../middlewares/error-handler');
+
 const server = express();
 
 server.use(express.json());
 server.use(cors());
+
+server.use(createUser);
+server.use(getUsers);
+server.use(userTransactions);
+server.use(errorHandler);
 
 module.exports = server;
