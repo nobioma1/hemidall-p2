a934ec5f0e18daffae166568895ab23fba1c4cc9
Noble Obioma <nobioma1@gmail.com>
2020-05-27 11:21:31 +0100

    Create function to randomly generate integer b/w min value and max value

--- server/worker/credit-debit-users.js
+++ server/worker/credit-debit-users.js
@@ -7,3 +7,14 @@ const axiosInstance = axios.create({
   baseURL: 'http://localhost:5000/api/users',
 });
 
+/**
+ * Generate an integer between minimum and maximum integer
+ * @param {*} max - maximum integer
+ * @param {*} min - minimum integer, set to default 0
+ *
+ * @returns number
+ */
+const generateRandom = (max, min = 0) => {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+};
+
